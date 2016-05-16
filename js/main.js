26ce331d7ac9d1cf729021dfc3f6d6c2d5372fed
Hector Huertas <hectorhuertas@gmail.com>
2016-05-16 16:28:41 +0200

    load tasks

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 $(document).ready(function () {
-  console.log('bbob');
   $('form').on('submit', addTask);
+  $('#pending').delegate('button.close', 'click', deleteTask);
+
+  loadTasks();
 });
+
+function deleteTask(e) {
+  // debugger
+  console.log('delete');
+  $(e.target).closest('.task').remove();
+}
+
+function loadTasks() {
+  var tasks = JSON.parse(localStorage.tasks || '{}');
+  html = '';
+  for (var task in tasks) {
+    // debugger
+    html = html + taskHtml(tasks[task]);
+  }
+  $('#pending').append(html);
+}
 
 function addTask(e) {
   e.preventDefault();
@@ -23,10 +41,12 @@ function saveTask(text) {
 }
 
 function taskHtml(text){
-  return  '<div class="col-xs-6">' +
+  return  '<div id="' + slugify(text) + '"class="task col-xs-6">' +
             '<div class="card card-block card-warning card-inverse">' +
               '<button class="close"><span>&times;</span></button>' +
-              '<h3 class="card-title">' + text + '</h3> </div> ';
+              '<h3 class="card-title">' + text + '</h3>' +
+            '</div>' +
+          '</div>';
 }
 
 function slugify(text){
